555bd0974fbce1f2d97966ecee22eba098b7dc7e
Charles Hill <s+github@chillweb.net>
2014-11-29 12:37:39 -0500

    fix default icon in config screen

--- icon-data.js
+++ icon-data.js
@@ -2,11 +2,33 @@ var yaml = require('js-yaml');
 var fs = require('fs');
 var cli = require('cli');
 
-module.exports.catalogMax = 60;
-module.exports.icons = { 'default': [{
-	name: 'Default',
-	classes: 'cardBack'
-}]};
+module.exports.categoryMax = 999;
+module.exports.icons = {};
+
+module.exports.addIcon = function(category, name, classes) {
+	var cat = category;
+	if(!module.exports.icons[cat]) {
+		module.exports.icons[cat] = [];
+	}
+
+	for (var x = 2; x < 99; x++) {
+		if(module.exports.icons[cat].length < module.exports.categoryMax) {
+			break;
+		}
+
+		cat = category + ' ' + x;
+
+		if(!module.exports.icons[cat]) {
+			module.exports.icons[cat] = [];
+		}
+	}
+
+
+	module.exports.icons[cat].push({
+		name: name,
+		classes: classes
+	}); 
+};
 
 module.exports.loadYaml = function(src) {
 	var iconList = yaml.safeLoad(fs.readFileSync(src, 'utf8'));
@@ -15,37 +37,14 @@ module.exports.loadYaml = function(src) {
 		var icon = iconList.icons[i];
 
 		for (var c = 0; c < icon.categories.length; c++) {
-			var catalog = icon.categories[c];
-
-			if(!module.exports.icons[catalog]) {
-				module.exports.icons[catalog] = [];
-			}
-
-			for (var x = 2; x < 99; x++) {
-				if(module.exports.icons[catalog].length < module.exports.catalogMax) {
-					break;
-				}
-
-				catalog = icon.categories[c] + ' ' + x;
-
-				if(!module.exports.icons[catalog]) {
-					module.exports.icons[catalog] = [];
-				}
-			}
-
-
-			module.exports.icons[catalog].push({
-				name: icon.name,
-				classes: 'fa fa-fw fa-'+icon.id
-			}); 
-		};
-	};
+			module.exports.addIcon(icon.categories[c], icon.name, 'fa fa-fw fa-'+icon.id);
+		}
+	}
 
 	console.log('Loaded ' +iconList.icons.length+ ' icons from '+src);
 };
 
 module.exports.save = function(dest, prefix, suffix) {
-
 	console.log('Saving to ' +dest)
 	var categories = Object.getOwnPropertyNames(module.exports.icons).sort();
 
