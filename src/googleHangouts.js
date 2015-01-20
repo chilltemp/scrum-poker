070d2447766265048a0eb9b5a889a49fa8bea14d
Charles Hill <s+github@chillweb.net>
2015-01-19 23:39:35 -0500

    cleanup

--- src/googleHangouts.js
+++ src/googleHangouts.js
@@ -9,9 +9,6 @@ chillweb.Hangout = function(state) {
 	this.isReady = false;
 	this.myId = null;
 	this.participants = [];
-	// this.onUpdate = new chillweb.EventController(this, 'update'); // param: none
-	// this.onNewParticipant = new chillweb.EventController(this, 'newParticipant') // param: newParticipant
-	// this.commands = new chillweb.NamedEventController();
 	this.myState = _.cloneDeep(this._defaultState);
 };
 
@@ -34,7 +31,7 @@ chillweb.Hangout.prototype.sendMyState = function() {
 	this._log('sendState', this.myState);
 
 	if(this.isReady) {
-		gapi.hangout.data.setValue('!' + this.myId, JSON.stringify(this.myState));
+		gapi.hangout.data.setValue(this.myId, JSON.stringify(this.myState));
 	} // else, will auto-send when ready
 };
 chillweb.Hangout.prototype.sendMyState = _.debounce(chillweb.Hangout.prototype.sendMyState, 250);
@@ -74,14 +71,6 @@ chillweb.Hangout.prototype._setParticipants = function(participants, action) {
 			found = true;
 		});
 
-		// for (var s = $scope.people.length - 1; s >= 0; s--) {
-		// 	if( $scope.people[s].id === participants[e].id ) {
-		// 		found = true;
-		// 		$scope.people[s].hasAppEnabled = participants[e].hasAppEnabled;
-		// 		$scope.people[s].online = true;
-		// 	}
-		// }
-
 		if(!found) {
 			var newParticipant = { $state: _.cloneDeep(this._defaultState)};
 			_.merge(newParticipant, current);
@@ -89,12 +78,6 @@ chillweb.Hangout.prototype._setParticipants = function(participants, action) {
 
 			this.emit('beforeAddParticipant', newParticipant);
 			this.participants.push(newParticipant);
-			// 	id: participants[e].id,
-			// 	displayName: participants[e].person.displayName,
-			// 	image: participants[e].person.image.url,
-			// 	state: createDefaultState(),
-			// 	online: true
-			// });
 		}
 	} // for: eventObj.participants
 
@@ -110,29 +93,6 @@ chillweb.Hangout.prototype._setParticipants = function(participants, action) {
 	}
 }; //_setParticipants
 
-// chillweb.Hangout.prototype._setState = function(eventObj) {
-	// $scope.consensus = false;
-
-	// for (var e = eventObj.addedKeys.length - 1; e >= 0; e--) {
-
-	// 	if(eventObj.addedKeys[e].key === '!resetAll') {
-	// 		if(eventObj.addedKeys[e].value != $scope.lastReset) {
-	// 			$scope.resetMe(eventObj.addedKeys[e].value);
-	// 		}
-	// 	} else if(eventObj.addedKeys[e].key === '!reveal') {
-	// 		var r = JSON.parse(eventObj.addedKeys[e].value);
-	// 		$scope.applyReveal(r);
-	// 	} else {
-	// 		for (var s = $scope.people.length - 1; s >= 0; s--) {
-	// 			if( $scope.people[s].id === eventObj.addedKeys[e].key ) {
-	// 				$scope.people[s].state = JSON.parse(eventObj.addedKeys[e].value);
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// $scope.update();
-// };
 
 chillweb.googleHangouts.provider('hangout', function() {
 
@@ -146,15 +106,14 @@ chillweb.googleHangouts.provider('hangout', function() {
 
 		gapi.hangout.onApiReady.add(function(eventObj){		
 			hangout._log('onApiReady', eventObj);
-			// try {
+
+			try {
 				if (eventObj.isApiReady) {
-
-
 					var participants = gapi.hangout.getParticipants();
 					var state = gapi.hangout.data.getState();
 
 					hangout._setParticipants(participants, 'sync');
-					hangout.myId = gapi.hangout.getLocalParticipantId();
+					hangout.myId = '!' + gapi.hangout.getLocalParticipantId();
 
 					hangout.isReady = true;
 					while(hangout._initQueue.length) {
@@ -165,23 +124,10 @@ chillweb.googleHangouts.provider('hangout', function() {
 					hangout.sendMyState();
 					hangout.emit('onApiReady');
 					hangout._emitUpdate();
-
-					// Create a similar object to the state change event
-				// 	var e = { addedKeys: [] };
-				// 	for(var key in state) {
-				// 		if (state.hasOwnProperty(key)) {
-				// 			e.addedKeys.push({ key: key, value: state[key] });
-				// 		}
-				// 	}
-				// 	$scope.applyStateChange(e);
-				// }
-
-				// $scope.update();
-				// $scope.autoSizeMainList();
 				}
-			// } catch(e) {
-			// 	hangout._log('onApiReady', e, e.stack);
-			// }
+			} catch(e) {
+				hangout._log('onApiReady', e, e.stack);
+			}
 		});
 
 		gapi.hangout.data.onStateChanged.add(function(eventObj) {
@@ -205,8 +151,7 @@ chillweb.googleHangouts.provider('hangout', function() {
 				}
 
 				hangout._emitUpdate();
-				// $scope.applyStateChange(eventObj);
-				} catch(e) {
+			} catch(e) {
 				hangout._log(e);
 			}
 		});
@@ -216,27 +161,10 @@ chillweb.googleHangouts.provider('hangout', function() {
 				hangout._log('onParticipantsChanged', eventObj);
 				hangout._setParticipants(eventObj.participants, 'sync');
 				hangout._emitUpdate();
-				// $scope.applyParticipants(eventObj.participants);
-				// $scope.update();
-				// $scope.autoSizeMainList();
 			} catch(e) {
 				hangout._log(e);
 			}
 		});
-
-		// gapi.hangout.onParticipantsRemoved.add(function(eventObj) {
-		// 	for (var e = eventObj.removedParticipants.length - 1; e >= 0; e--) {
-		// 		for (var s = $scope.people.length - 1; s >= 0; s--) {
-		// 			if(eventObj.removedParticipants[e].id == $scope.people[s].id) {
-		// 				$scope.people[s].online = false;
-		// 			}
-		// 		}
-		// 	}
-
-		// 	$scope.update();
-		// 	$scope.autoSizeMainList();
-		// });
-
 
 		return hangout;
 	};
